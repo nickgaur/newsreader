7dc16c1686be146f8bf368485276c76797669427
nickgaur <67472332+nickgaur@users.noreply.github.com>
2021-01-15 23:36:33 +0530

    Update NewsReader.py

--- NewsReader.py
+++ NewsReader.py
@@ -11,9 +11,8 @@ if __name__ == '__main__':
 
     news_api = requests.get("https://newsapi.org/v2/top-headlines?",
                             params = {"country": "in", "from": "2021-01-14", "to": "2021-01-14", "language": "en", "pageSize": 100,
-                                      "apiKey": "0c8b8ea8354d4f58a9d3c522df60e04a"})
+                                      "apiKey": "0c8b***********************f60e04a"})
 
-    # print(news_api.text)
     news_dict = news_api.json()
     for item in news_dict["articles"]:
         speak(item["description"])
